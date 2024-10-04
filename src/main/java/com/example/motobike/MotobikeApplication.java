a42d9ca67cb791c3d9fb0ea28e9343e7b00f7701
nguyen <nguyenthcs430@gmail.com>
2024-10-04 18:23:43 +0700

    config jsp

--- src/main/java/com/example/motobike/MotobikeApplication.java
+++ src/main/java/com/example/motobike/MotobikeApplication.java
@@ -3,7 +3,8 @@ package com.example.motobike;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class MotobikeApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/motobike/controller/client/HomePageController.java
+++ src/main/java/com/example/motobike/controller/client/HomePageController.java
@@ -1,15 +1,16 @@
 package com.example.motobike.controller.client;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomePageController {
 
-    @GetMapping("/")
-    public String Hello() {
-        return "Hello";
+    @GetMapping("/hello")
+    public String Hello(Model model) {
+        return "hello";
     }
-   
+
 }
