4101e7526bbf7f69b4efa847ca3ea6b6e7d2356c
nguyen <nguyenthcs430@gmail.com>
2024-10-18 18:56:56 +0700

    delete product from cart

--- src/main/java/com/example/motobike/controller/client/ItemController.java
+++ src/main/java/com/example/motobike/controller/client/ItemController.java
@@ -1,11 +1,16 @@
 package com.example.motobike.controller.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.motobike.domain.Cart;
+import com.example.motobike.domain.CartDetail;
 import com.example.motobike.domain.Product;
 import com.example.motobike.domain.User;
 import com.example.motobike.service.ProductService;
@@ -48,8 +53,30 @@ public class ItemController {
     public String getCartPage(Model model, HttpServletRequest request) {
         User currentUser = new User();
         HttpSession session = request.getSession();
-        System.out.println(">>>>>>" + session);
+        long id = (long) session.getAttribute("id");
+        currentUser.setId(id);
+
+        Cart cart = this.productService.fetchByUser(currentUser);
+        List<CartDetail> cartDetails = cart == null ? new ArrayList<CartDetail>() : cart.getCartDetails();
+        double totalPrice = 0;
+        for (CartDetail cd : cartDetails) {
+            totalPrice += cd.getPrice() * cd.getQuantity();
+        }
+        model.addAttribute("cartDetails", cartDetails);
+        model.addAttribute("totalPrice", totalPrice);
+
+        model.addAttribute("cart", cart);
 
         return "client/cart/show";
     }
+
+    @PostMapping("/delete-cart-product/{id}")
+    public String deleteCartDetail(@PathVariable long id, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        long cartDetailId = id;
+        this.productService.handleRemoveCartDetail(cartDetailId, session);
+        return "redirect:/cart";
+    }
+
+    @P
 }
